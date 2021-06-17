24d29941719f8760a350b20cbe3089292930687a
lyz7805 <lyz7805@gmail.com>
2021-06-17 17:31:10 +0800

    更新topthink/framework版本为v5.1.41

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitd7a1a5f41e2fb6148a8b8c876faba931
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
